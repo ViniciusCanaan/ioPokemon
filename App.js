7d40e388ed5211824b26bc1289a709465e8c33af
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-03-01 17:23:33 -0300

    Adicionando SplashScreen

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import {View, Text} from 'react-native';
 import CardPokemon from "./src/components/CardPokemon";
@@ -8,12 +8,20 @@ import Statistics from "./src/screens/Statistics";
 import Home from "./src/screens/Home";
 import Favorits from "./src/screens/Favorits";
 
+import SplashScreen from 'react-native-splash-screen';
+
 import MainStack from "./src/navigators/MainStack";
 
 import { NavigationContainer } from '@react-navigation/native';
 
 
 const App = () =>{
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
+
+
     return(
       <NavigationContainer>
         <MainStack/>
--- android/app/src/main/java/com/iopokemon/MainActivity.java
+++ android/app/src/main/java/com/iopokemon/MainActivity.java
@@ -1,6 +1,9 @@
 package com.iopokemon;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +11,14 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+   protected void onCreate(Bundle savedInstanceState){
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+   }
   @Override
   protected String getMainComponentName() {
+
     return "ioPokemon";
   }
 }
